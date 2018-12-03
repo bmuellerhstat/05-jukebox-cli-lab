c89ee6a80a93348acb2a9fefeb0280312010835b
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-12-03 18:44:06 +0000

    ec

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,29 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
 
+def list(songs)
+  for i in 0..songs.length
+    puts "#{i+1}. #{songs[i]}"
+  end
+end
+def play(songs) 
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+  for i in 0..songs.length
+    if song ==songs[i]  
+      puts "Playing #{song}"
+    end
+  end
+end
+def exit_jukebox
+  puts "Goodbye"
+end
+play(songs)

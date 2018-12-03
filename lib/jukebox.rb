1ef2a42e936354755fd3485dc734459046eb9a3b
Wei Zheng <weiz9762@hstat.org>
2018-12-03 23:26:05 +0000

    pass all the rspec

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  songs.each_with_index do |song, index|
+    if user_input == song
+      puts "Playing #{song}"
+    elsif user_input.to_i < index
+      select_song = songs[user_input.to_i - 1]
+      puts "Playing #{select_song}" 
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts help
+  
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    case command
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+    break if command == "exit"
+    end
+  end
+end
+

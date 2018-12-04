e4891f5345a02f2a57e11f37667e0c81e743b006
Yu Hang Chen <yuhangc9321@hstat.org>
2018-12-04 01:56:26 +0000

    define methods to pass tests

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,6 +7,56 @@ songs = [
   "The Cults - Abducted",
   "Phoenix - Consolation Prizes",
   "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
+  "Amos Lee - Keep It Loose, Keep It Tight",
 ]
 
+def help
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs_array)
+  songs_array.each.with_index(1) do |song,index|
+    puts "#{index}. #{song}"
+  end
+end
+
+def play(songs_array)
+  puts "Please enter a song name or number:"
+  song_request = gets.chomp
+  
+  if songs_array.index(song_request) != nil
+    puts "Playing #{songs_array[songs_array.index(song_request)]}"
+  elsif /[0-9]/ =~ song_request && songs_array[song_request.to_i-1] != nil
+    puts "Playing #{songs_array[song_request.to_i-1]}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    user_command = gets.chomp
+    
+    case user_command
+    when "exit"
+      exit_jukebox
+      break
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    end
+  end
+end
